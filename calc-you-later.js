2c5e0e0aceebef232529003ccf45854a4c76f05b
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-10-29 14:36:11 -0400

    finished the calculator

--- calc-you-later.js
+++ calc-you-later.js
@@ -1,20 +1,24 @@
 
-//alert('Please enter your operation followed by your number variables');
+
 let what = prompt('Please enter your operation followed by your number variables');
-const arr = []
-for(let i =0; i < what.length; i++){
-    arr.push(what[i])
 
-}
+const arr = what.split(' ')
+// for(let i =0; i < what.length; i++){
+//     if(what[i] !== ' '){
+//         arr.push(what[i])
 
+//     }
+    
+
+// }
+
+// console.log(what.split(' '))
+// arr.push(what.split(' '))
 const operation = arr[0]
-const num1 = Number(arr[1])
-const num2 = Number(arr[2])
+const num1 = arr[1]
+const num2 = arr[2]
 
  const result = calculate(operation, num1, num2)
   
 
  document.querySelector('#result').innerHTML = result
-// const reaction = document.querySelector('#result')
-
-// onclick(reaction, result)
