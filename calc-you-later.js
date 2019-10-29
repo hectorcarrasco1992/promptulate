44751991cc81d71aea58bbe7dfeb45afab012346
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-10-29 14:11:47 -0400

    calculator can do math for single diits only

--- calc-you-later.js
+++ calc-you-later.js
@@ -1,3 +1,20 @@
-// const result = calculate(operation, num1, num2)
 
-alert('Please enter your operation followed by your number variables');
+//alert('Please enter your operation followed by your number variables');
+let what = prompt('Please enter your operation followed by your number variables');
+const arr = []
+for(let i =0; i < what.length; i++){
+    arr.push(what[i])
+
+}
+
+const operation = arr[0]
+const num1 = Number(arr[1])
+const num2 = Number(arr[2])
+
+ const result = calculate(operation, num1, num2)
+  
+
+ document.querySelector('#result').innerHTML = result
+// const reaction = document.querySelector('#result')
+
+// onclick(reaction, result)
